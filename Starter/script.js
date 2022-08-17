689a0be8da4d801d2cc40f930e93eed2293486ac
Nick Stevens <nickstevens@Nicks-MacBook-Pro.local>
2022-08-17 16:41:00 -0400

    wrote pseudocode, successfully created if statement to bring correct prompts in correct order, started working on storing user confirm choices to an array

--- Starter/script.js
+++ Starter/script.js
@@ -1,20 +1,108 @@
 // 1. Write a for loop to log the individual elements in numeric array
-var numbersArray = [ 1, 2, 3, 4, 5, 6, 7 ];
-console.log(numbersArray);
-
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+for (let i = 0; i < numbersArray.length; i++) {
+  console.log(numbersArray[i]);
+}
 
 // 2. Write a for loop to log the individual elements in lower case alphabets
-var lowerCaseAlphabetsArray = [ 'a', 'b' ,'c', 'd', 'e', 'f' ];
-console.log(lowerCaseAlphabetsArray);
-
+let lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+for (let i = 0; i < lowerCaseArray.length; i++) {
+  console.log(lowerCaseArray[i]);
+}
 
 // 3. Store array of numeric characters to a variable and log the individual elements to the console using for-loop
 
-
 // 4. Store array of all lowercase alphabets to a variable and log the individual elements to the console using for-loop
-
 
 // 5. Store array of all uppercase alphabets to a variable and log the individual elements to the console using for-loop
 
-
 // 6. Store array of special characters to a variable and log the individual elements to the console using for-loop
+// let lowerCaseArray = [
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+//   "g",
+//   "h",
+//   "i",
+//   "j",
+//   "k",
+//   "l",
+//   "m",
+//   "n",
+//   "o",
+//   "p",
+//   "q",
+//   "r",
+//   "s",
+//   "t",
+//   "u",
+//   "v",
+//   "w",
+//   "x",
+//   "y",
+//   "z",
+// ];
+let upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let specialCharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+// let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+console.log(lowerCaseArray);
+console.log(upperCaseArray);
+console.log(specialCharArray);
+console.log(numbersArray);
--- script.js
+++ script.js
@@ -1,10 +1,189 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-let lowerCase = ["a", "b"];
-let upperCase = ["A", "B"];
-let specialChar = ["!", "@", "#"];
-let numericalChar = ["1", "2", "3"];
+// let choices = {
+//   lowerCase: [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//   ],
+//   upperCase: [
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//   ],
+//   specialChar: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"],
+//   numberChar: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+// }
+
+//TODO: Declare functions containing arrays of each parameter
+let lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let specialCharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// let selectedChar = "";
+let userChoice = [];
+
+//? Make password between 8-20 characters in a better way?
+//? let passwordLength = Math.floor(Math.random * userInput);
+
+// TODO: Make function generatePassword(); receive user inputs upon clicking Generate (click event already defined)
+function generatePassword() {
+  let userInput = prompt(
+    "How long would you like your password to be? (Must Be Between 8-20 Characters)"
+  );
+  if (userInput >= 8 && userInput <= 20) {
+    console.log("Valid Value");
+
+    function confirmLower() {
+      confirm("Would you like to include lower case letters in your password?");
+    }
+    confirmLower();
+    if (confirmLower) {
+      //TODO:  Add the lowerCaseArray to the possible outcomes randomness.
+      let selectedChar = userChoice.push(...lowerCaseArray);
+      console.log(selectedChar);
+      function confirmUpper() {
+        confirm(
+          "Would you like to include UPPER case letters in your password?"
+        );
+      }
+      if (confirmUpper) {
+        let selectedChar = selectedChar.push(...upperCaseArray);
+        function confirmSpecial() {
+          confirm(
+            "Would you like to include special Characters in your password?"
+          );
+        }
+        if (confirmSpecial) {
+          let selectedChar = selectedChar.push(...specialCharArray);
+          function confirmNumbers() {
+            confirm("Would you like to include numbers in your password");
+          }
+          if (confirmNumbers) {
+            let selectedChar = selectedChar.push(...numbersArray);
+          } else {
+            return;
+          }
+        } else {
+          //TODO: if no special characters make a prompt confirmNumbers and if confirm selectedChar does not include special characters.
+          confirmNumbers();
+        }
+      } else {
+        //TODO: if no uppercase make prompt confirmSpecial and if confirm selectedChar does not include uppercase
+        confirmSpecial();
+      }
+    } else {
+      //TODO: if no lowercase make prompt confirmUpper and if confirm selectedChar does not include lowercase
+      confirmUpper();
+    }
+
+    // getLowerCase(); TODO: Make this function activate the lowerCaseArray.
+  } else {
+    window.alert("Invalid Value");
+    generatePassword();
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
